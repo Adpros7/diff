72a29a447a213685048fc9e941899d72a8600c9f
Adpros7 <pranit.advik@gmail.com>
2026-02-22 20:37:33 -0500

    Refactor word processing to sort letter pairs alphabetically.

--- main.py
+++ main.py
@@ -2,15 +2,26 @@ sample_input: str = """abcddef vdfaddwf
 friends afraid
 delicious indiginous
 shalom saaalaaam
-happy shipshape
-"""
+happy shipshape"""
+
+ALPHABET: str = "abcdefghijklmnopqrstuvwxyz"
+
+
+def sort_by_alphabet(s: str) -> int:
+    return ALPHABET.find(s)
+
+
 def main(input: str = sample_input):
-    words = [sorted(v.split(maxsplit=1), key=len) for v in input.split("\n")]
+    words: list[list] = [sorted(v.split(), key=len) for v in input.split("\n")]
+
     for i, v in enumerate(words):
         if len(v[0]) != len(v[1]):
-            words[i][1] = words[i][1][:len(v[0])]
+            words[i][1] = words[i][1][: len(v[0])]
 
-
+    for i in words:
+        for x, y in zip(i[0], i[1]):
+            pass
+        
     return words
 
 
